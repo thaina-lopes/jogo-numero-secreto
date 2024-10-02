9169b70c4d7cf4255d15ec02d6476a32540b258a
Thaina <thainalopes@gmail.com>
2024-10-02 17:25:29 -0300

    Adiciona a função de gerar um número aleatório entre 0 e 100

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 alert("Bem vindo ao jogo do número secreto!");
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 let chute;
+let tentativas = 1;
 
 // enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto) {
-  chute = prompt("Escolha um número entre 1 e 10");
+  chute = prompt("Escolha um número entre 1 e 100");
   // se chute for igual ao número secreto
   if (chute == numeroSecreto) {
-    alert(`Você acertou! O número secreto é ${numeroSecreto}`);
+    break;
   } else {
     // se chute for maior ou menor que o número secreto
     if (chute > numeroSecreto) {
@@ -15,5 +16,11 @@ while (chute != numeroSecreto) {
     } else {
       alert(`Você errou! O número secreto é maior que ${chute}`);
     }
+    tentativas++;
   }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Aee! O número secreto é ${numeroSecreto}! Você acertou com ${tentativas} ${palavraTentativa}`
+);
